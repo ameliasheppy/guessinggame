32e4f3e901628894302b23f714ff45d21901cbad
Amy Stanley <thegirl033007@gmail.com>
2023-02-27 16:54:31 -0500

    initializing repo

--- game.py
+++ game.py
@@ -10,17 +10,26 @@ def guessing_game():
     # throwing in a print to see if it gives us a random number
     counter = 1
     number = random.randint(1, 100)
+    print(number)
     name = input("Howdy! What's your name?")
-    print(f"{name}, I'm thiniking of a number between 1 and 100 \n Try to guess my number!")
+    print(f"{name}, I'm thinking of a number between 1 and 100 \n Try to guess my number!")
+    guess = 'cats'
 
-    guess = int(input("Your guess? "))
     while guess != number:
-        if guess > number:
-            print("Your guess is too high, try again!")
+        try:
+            guess = int(input("Your guess? "))
+        except ValueError:
+            print('Please enter a valid number')
+            continue
+        if guess < 101 and guess > 0:
+            if guess > number:
+                print("Your guess is too high, try again!")
+            else:
+                print("Your guess is too low, try again!")
         else:
-            print("Your guess is too low, try again!")
-        guess = int(input("Your guess? "))
+            print('Sorry, you need to give a number from 1-100!')
         counter += 1
+
     print(f"Well done! You found my number in {counter} tries :)")
 
 
