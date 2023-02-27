1c659385fdbfe2ad3f727bb379b56794269bb20d
Amy Stanley <thegirl033007@gmail.com>
2023-02-27 16:24:35 -0500

    added in the conditional to guess a number and the first user number input

--- game.py
+++ game.py
@@ -1,9 +1,23 @@
 """A number-guessing game."""
 
 # Put your code here
+import random
 
 print('hi!')
 
 
 def guessing_game():
-    secret_number = int(input("Guess a number between 1 and 15"))
+    # throwing in a print to see if it gives us a random number
+    number = random.randint(1, 100)
+    print(number)
+    name = input("Howdy! What's your name?")
+    print(f"{name}, I'm thiniking of a number between 1 and 100 \n Try to guess my number!")
+
+    guess = int(input("Your guess? "))
+    if guess == number:
+        print(f"Well done, {name}, you found my number!")
+    else:
+        print("That's the wrong number!")
+
+
+guessing_game()
