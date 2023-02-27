62b477c66136d8e34462aca59eccfae8590ea00c
Amy Stanley <thegirl033007@gmail.com>
2023-02-27 17:36:05 -0500

    added in an array to hold scores

--- game.py
+++ game.py
@@ -5,16 +5,19 @@ import random
 
 print('hi!')
 
+#
+name = input("Howdy! What's your name?")
+print(f"{name}, I'm thinking of a number between 1 and 100 \n Try to guess my number!")
+
 
 def guessing_game():
     # throwing in a print to see if it gives us a random number
-    counter = 1
+    counter = 0
     number = random.randint(1, 100)
     print(number)
-    name = input("Howdy! What's your name?")
-    print(f"{name}, I'm thinking of a number between 1 and 100 \n Try to guess my number!")
     guess = 'cats'
     max_count = 3
+
     while guess != number and counter <= max_count:
         try:
             guess = int(input("Your guess? "))
@@ -30,10 +33,14 @@ def guessing_game():
             print('Sorry, you need to give a number from 1-100!')
         counter += 1
 
-    if counter >= max_count:
-        print('OOPS! You exceeded the max number of attemped guesses, please start over.')
-    else:
+    if guess == number:
         print(f"Well done! You found my number in {counter} tries :)")
+    else:
+        print('OOPS! You exceeded the max number of attemped guesses, please start over.')
+    return counter
 
 
-guessing_game()
+scores = []
+scores.append(guessing_game())
+
+print(scores)
